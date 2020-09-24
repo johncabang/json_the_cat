c9917bd4d6beb3aba8354cd96de82a3696304588
johncabang <johncabz@gmail.com>
2020-09-24 19:18:37 +0000

    Edge case: Breed Not Found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,26 @@
 const request = require('request');
-
-// request('http://example.edu/', (error, response, body) => {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+const breed = process.argv.slice(2);
 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
   // console.log('error:', error);
   // console.log('statusCode:', response && response.statusCode);
   // console.log('body', body);
   // console.log(typeof body);
-  const data = JSON.parse(body);
   // console.log(data);
   // console.log(typeof data);
-  console.log(data[0].description);
-});
+  // console.log(data[0].description);
+
+
+const breedDescription = function(breed) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    let breedName = data[0].name;
+    // let breedDescrip = data[0].description;
+
+    if(breed !== breedName) {
+      console.log('Sorry breed name is not found.');
+    }
+  });
+};
+
+breedDescription(breed);
